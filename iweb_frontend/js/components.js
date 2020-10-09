bc7070b910ad5fd301273c7290a26b6b52f8b864
bao1224 <70950000+bao1224@users.noreply.github.com>
2020-10-09 09:21:32 +0800

    完成登录页面，以及响应式布局

--- iweb_frontend/js/components.js
+++ iweb_frontend/js/components.js
@@ -1,13 +1,16 @@
-/* 响应式导航条：点击汉堡包按钮时导航菜单从左侧进入；点击关闭按钮时导航隐藏到左侧 */
-//根据属性选择器查找下一个元素
+/*1.响应式导航条的显示和隐藏*/
+//点击汉堡包按钮时导航菜单从左侧进入
 document.querySelector('[data-toggle="nav"]').onclick = function(){
-	// alert(123)
-	// 将导航条从左侧移进来——给navbar添加active类
+	//将导航条从左侧移进来——给navbar添加active类
 	document.querySelector('[data-target="nav"]').classList.add('active')
 }
+
+//点击关闭按钮时导航隐藏到左侧
 document.querySelector('[data-close="nav"]').onclick = function(){
-	// 将导航条从左侧移出去——给navbar添加active类
+	//将导航条从左侧移出去——给navbar去掉active类
 	document.querySelector('[data-target="nav"]').classList.remove('active')
 }
+
+
 
 
